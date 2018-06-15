2bc7a01547ce9cfd6c88096eb3db62e81606bb45
David Chen <davidchen.kyc@gmail.com>
2018-06-15 11:35:01 -0400

    renamed udf

--- src/main/scala/org/nmdp/spark/hml/Hml.scala
+++ src/main/scala/org/nmdp/spark/hml/Hml.scala
@@ -21,20 +21,20 @@ object Hml {
 
     val typeRegex = "ArrayType.*".r
 
-    val hlaGlaStringParser= udf{
+    val multipleGlStringParser= udf{
       glstrings: WrappedArray[String] => glstrings.flatMap(x => x.split(glStringRegexp))
     }
 
-    val kirGlaStringParser= udf{
+    val singleGlStringParser= udf{
       glstring: String => glstring.split(glStringRegexp)
     }
 
     //Use regex to find the data type of column typing.allele-assignment.glstring. 
-    //If it's an array, use hlaGlaStringParser. Otherwise, use kirGlaStringParser
-    
+    //If it's an array, use multipleGlStringParser. Otherwise, use singleGlStringParser
+
     df.select("typing.allele-assignment.glstring").dtypes(0)._2 match {
-      case typeRegex() => df.withColumn("alleles", hlaGlaStringParser(col("typing.allele-assignment.glstring")))
-      case _ => df.withColumn("alleles", kirGlaStringParser(col("typing.allele-assignment.glstring")))
+      case typeRegex() => df.withColumn("alleles", multipleGlStringParser(col("typing.allele-assignment.glstring")))
+      case _ => df.withColumn("alleles", singleGlStringParser(col("typing.allele-assignment.glstring")))
 
     }
   }
