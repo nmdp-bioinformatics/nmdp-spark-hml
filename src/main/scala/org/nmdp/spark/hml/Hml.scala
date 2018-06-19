ffda4a985904ebc62fbfd05e79d014bbc968ace5
Mike Trepanier <michael.p.trepanier@gmail.com>
2018-06-19 09:16:28 -0700

    provided-dependencies (#3)
    * fix glstring parser with udf and flatmap
    
    * corrected class import and column processing
    
    * added pattern match to handle two types of schema
    
    * added comment
    
    * renamed udf
    
    * provided

--- src/main/scala/org/nmdp/spark/hml/Hml.scala
+++ src/main/scala/org/nmdp/spark/hml/Hml.scala
@@ -1,7 +1,7 @@
 package org.nmdp.spark.hml
 
 import org.apache.spark.sql.{DataFrame, SQLContext}
-import org.apache.spark.sql.functions.{ split, col, udf }
+import org.apache.spark.sql.functions.{ col, udf }
 import scala.collection.mutable.WrappedArray
 
 
@@ -15,28 +15,23 @@ object Hml {
       .option("rootTag", "hml")
       .option("rowTag", "sample")
       .load(hmlFile)
-
-
     val glStringRegexp = "\\^|\\||\\+|\\~|\\/"
-
     val typeRegex = "ArrayType.*".r
-
     val multipleGlStringParser= udf{
       glstrings: WrappedArray[String] => glstrings.flatMap(x => x.split(glStringRegexp))
     }
-
     val singleGlStringParser= udf{
       glstring: String => glstring.split(glStringRegexp)
     }
-
     //Use regex to find the data type of column typing.allele-assignment.glstring. 
     //If it's an array, use multipleGlStringParser. Otherwise, use singleGlStringParser
-
-    df.select("typing.allele-assignment.glstring").dtypes(0)._2 match {
+    val newDF = df.select("typing.allele-assignment.glstring").dtypes(0)._2 match {
       case typeRegex() => df.withColumn("alleles", multipleGlStringParser(col("typing.allele-assignment.glstring")))
       case _ => df.withColumn("alleles", singleGlStringParser(col("typing.allele-assignment.glstring")))
-
     }
+    val cols = newDF.columns
+    val renamedCols = cols.map(x => x.replace("_", ""))
+    newDF.toDF(renamedCols: _*)
   }
 
 }
